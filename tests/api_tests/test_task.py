76b74a504cf7a3c0ad4bd3b419b43d31cf748581
ilya.kozlov <ilya.kozlov2@yandex.ru>
2026-02-11 23:20:38 +0100

    add test on tasks and test tasts update

--- tests/api_tests/test_task.py
+++ tests/api_tests/test_task.py
@@ -2,9 +2,11 @@ import time
 
 import pytest
 import requests
-from pygments.lexer import words
+
 
 from schemas.tasks.match_word_explanation import MatchWordExplanation
+from schemas.tasks.sentence_with_placeholder import SentenceWithPlaceholder
+from schemas.tasks.word2explanation import Word2Explanation
 
 
 @pytest.fixture
@@ -26,13 +28,27 @@ def test_smoke(fill_db: None) -> None:
 
 def test_tasks(fill_db: None) -> None:
     params = {"uid": "test"}
-    response = requests.get("http://localhost:2218/tasks/tasks", params=params)
-    response.raise_for_status()
+    _get_tasks(params)
 
 
 def test_task_type(fill_db: None) -> None:
     params = {"uid": "test", "task_type": "MatchWordExplanation"}
+    raw_answer = _get_tasks(params)
+    MatchWordExplanation.model_validate(raw_answer)
+
+def test_task_type1(fill_db: None) -> None:
+    params = {"uid": "test","task_type": "SentenceWithPlaceholder" }
+    raw_answer = _get_tasks(params)
+    SentenceWithPlaceholder.model_validate(raw_answer)
+
+
+def test_task_type2(fill_db: None)-> None:
+    params = {"uid":"test", "task_type":"Word2Explanation"}
+    raw_answer = _get_tasks(params)
+    Word2Explanation.model_validate(raw_answer)
+
+def _get_tasks(params:dict)->dict:
     response = requests.get("http://localhost:2218/tasks/tasks", params=params)
     response.raise_for_status()
     raw_answer = response.json()
-    MatchWordExplanation.model_validate(raw_answer)
+    return raw_answer
