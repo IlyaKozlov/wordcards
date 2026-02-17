22c1f75a4ab65aff6fc36e6ef2ef6b288f18a4b3
ilya.kozlov <ilya.kozlov2@yandex.ru>
2026-02-17 17:35:07 +0100

    add test_backups

---
+++ tests/api_tests/test_backups.py
@@ -0,0 +1,16 @@
+import os.path
+import tempfile
+import zipfile
+
+import requests
+
+
+def test_backups():
+    response = requests.get("http://localhost:2218/backups")
+    response.raise_for_status()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        zip_path = os.path.join(tmpdir, "fluffy.zip")
+        with open( zip_path, "wb") as fluffy:
+            fluffy.write(response.content)
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
+            zip_ref.namelist()
