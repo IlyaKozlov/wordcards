99f80cdb64923cea7b246ffa0e918a28cf0df9f0
juliaowl7-lang <julia.owl.7@gmail.com>
2026-02-23 20:27:28 +0100

    add new tests

--- tests/api_tests/test_show_new_word.py
+++ tests/api_tests/test_show_new_word.py
@@ -1,13 +1,15 @@
 import requests
 
-from scripts.translate import response
 
 
-def test_show_new_woed():
+
+def test_show_new_words():
     params = {"uid": "test"}
     response = requests.get("http://localhost:2218/uncover/show_new_word", params= params)
     response.raise_for_status()
-    data = response.json()
+    assert  response.status_code == 200
+    result = response.json()
 
-    assert "word" in data
-    assert data["word"] in ["red","blue","green","gray"]
+
+    assert "word" in result
+    assert len(result["word"])>0
