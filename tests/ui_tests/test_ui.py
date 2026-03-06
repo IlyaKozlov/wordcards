e7beaecc1fc120836aaf8b5b9dea9e1b349fc541
juliaowl7-lang <julia.owl.7@gmail.com>
2026-03-06 20:50:02 +0100

    add login test

--- tests/ui_tests/test_ui.py
+++ tests/ui_tests/test_ui.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
@@ -13,3 +15,23 @@ def test_icon_click():
     finally:
         driver.close()
     assert element is not None
+
+
+def test_login():
+    driver: WebDriver = webdriver.Chrome()
+    driver.get("http://localhost:2218/")
+    id = "login-link"
+    element = driver.find_element(By.ID, id)
+    assert  element is not None
+    element.click()
+    assert driver.current_url == "http://localhost:2218/static/login.html"
+
+    input_form = driver.find_element(By.ID, "uid")
+    input_form.send_keys("test")
+
+    submit = driver.find_element(By.ID, "okBtn")
+    submit.click()
+
+
+    driver.close()
+
