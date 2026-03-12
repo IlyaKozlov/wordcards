7a16566db57c3394d242363f9e88570204dec476
ilya.kozlov <ilya.kozlov2@yandex.ru>
2026-03-12 22:53:01 +0100

    change test_traning

--- tests/ui_tests/test_ui.py
+++ tests/ui_tests/test_ui.py
@@ -3,22 +3,18 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
+from utils_for_test import fill_db  # noqa
+from web_driver import chrome_web_driver as driver
 
 
-
-def test_icon_click():
-    driver: WebDriver = webdriver.Chrome()
-    try:
-        driver.get("http://localhost:2218/")
-        xpath = '//*[@id="main-rat"]'
-        element = driver.find_element(By.XPATH, xpath)
-    finally:
-        driver.close()
+def test_icon_click(driver):
+    driver.get("http://localhost:2218/")
+    xpath = '//*[@id="main-rat"]'
+    element = driver.find_element(By.XPATH, xpath)
     assert element is not None
 
 
-def test_login():
-    driver: WebDriver = webdriver.Chrome()
+def test_login(driver):
     driver.get("http://localhost:2218/")
     id = "login-link"
     element = driver.find_element(By.ID, id)
@@ -33,25 +29,26 @@ def test_login():
     submit.click()
 
 
-    driver.close()
+def test_training(fill_db: None, driver):
+      check_params("http://localhost:2218/static/task_uncover.html", "?uid=test", driver)
 
-def test_training():
-    driver: WebDriver = webdriver.Chrome()
-    driver.get("http://localhost:2218/?uid=test")
+def test_training_new_user(driver):
+    check_params("http://localhost:2218/static/no_new_words.html", "?uid=dvarf", driver)
+    href = "Main Page"
+    element = driver.find_element(By.LINK_TEXT, href)
+    assert element is not None
+    element.click()
+    assert driver.current_url == "http://localhost:2218/static/main.html"
+
+def check_params(expected_prefix, uid, driver):
+    expected = expected_prefix + uid
+    driver.get("http://localhost:2218/" + uid)
     id = "training-link"
     element = driver.find_element(By.ID, id)
     assert element is not None
     element.click()
-    #assert  driver.current_url == "http://165.154.213.241:2218/static/task_uncover.html?uid=test"
-
-    # driver: WebDriver = webdriver.Chrome()
-    # driver.get("http://localhost:2218/static/no_new_words.html?uid=test")
-    # href = "static/main.html"
-    # elements = driver.find_elements(By.LINK_TEXT,href )
-    # assert element is not None
-    # element.click()
-
-    driver.close()
+    time.sleep(1)
+    assert driver.current_url == expected
 
 
 def test_uncover():
