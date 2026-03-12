52c4f4ec31a14ac6caf962469326671f0a20a12a
juliaowl7-lang <julia.owl.7@gmail.com>
2026-03-12 15:26:09 +0100

    add: test_training, test_uncover, test_book_upload, test_translate

--- tests/ui_tests/test_ui.py
+++ tests/ui_tests/test_ui.py
@@ -35,3 +35,54 @@ def test_login():
 
     driver.close()
 
+def test_training():
+    driver: WebDriver = webdriver.Chrome()
+    driver.get("http://localhost:2218/?uid=test")
+    id = "training-link"
+    element = driver.find_element(By.ID, id)
+    assert element is not None
+    element.click()
+    #assert  driver.current_url == "http://165.154.213.241:2218/static/task_uncover.html?uid=test"
+
+    # driver: WebDriver = webdriver.Chrome()
+    # driver.get("http://localhost:2218/static/no_new_words.html?uid=test")
+    # href = "static/main.html"
+    # elements = driver.find_elements(By.LINK_TEXT,href )
+    # assert element is not None
+    # element.click()
+
+    driver.close()
+
+
+def test_uncover():
+    driver: WebDriver = webdriver.Chrome()
+    driver.get("http://localhost:2218/?uid=test")
+    id = "uncover-link"
+    element = driver.find_element(By.ID, id)
+    assert element is not None
+    element.click()
+
+    driver.close()
+
+def test_book_upload():
+    driver: WebDriver = webdriver.Chrome()
+    driver.get("http://localhost:2218/?uid=test")
+    id = "book-upload-link"
+    element = driver.find_element(By.ID, id)
+    assert element is not None
+    element.click()
+
+    driver.close()
+
+def test_translate():
+    driver: WebDriver = webdriver.Chrome()
+    driver.get("http://localhost:2218/?uid=test")
+    id = "translate-link"
+    element = driver.find_element(By.ID, id)
+    assert element is not None
+    element.click()
+
+    driver.close()
+
+
+
